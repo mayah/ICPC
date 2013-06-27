4aefce90b88008b5b538ef0ef63f519543cb2e91
MAYAH <mayah@mayah.jp>
2013-06-27 21:10:06 +0900

    Style fix

--- JapanDomestic/2003-Aizu/C/C.cc
+++ JapanDomestic/2003-Aizu/C/C.cc
@@ -4,79 +4,53 @@
 
 using namespace std;
 
+string solve(const vector<string>& field, int H, int W)
+{
+    vector<vector<string> > work(H + 1, vector<string>(W + 1));
+    
+    for (int h = H - 1; h >= 0; --h) {
+        for (int w = W - 1; w >= 0; --w) {
+            if (!isdigit(field[h][w]))
+                continue;
+
+            // 下と右のうち、長い方を選択
+            if ((work[h][w + 1].size() > work[h + 1][w].size()) ||
+                ((work[h][w + 1].size() == work[h + 1][w].size()) &&
+                 work[h][w + 1] > work[h + 1][w])) {
+                work[h][w] = field[h][w] + work[h][w + 1];
+            } else {
+                work[h][w] = field[h][w] + work[h + 1][w];
+            }
+        }
+    }
+
+    // 全て計算し終わったので、最も大きいものを探す。このときは先頭の 0 は取り除く。
+    string result;
+    for (int h = 0; h < H; ++h) {
+        for (int w = 0; w < W; ++w) {
+            if (work[h][w].find_first_not_of('0') != string::npos)
+                work[h][w] = work[h][w].substr(work[h][w].find_first_not_of('0'));
+            if (work[h][w].size() > result.size() || (work[h][w].size() == result.size() && work[h][w] > result))
+                result = work[h][w];
+        }
+    }
+    
+    return result;
+}
+
 int main(void)
 {
 	int W, H;
 	while (cin >> W >> H, (W || H)) {
-		string str;
-		getline(cin, str); // chop \n
-		// read field.
+        // 番兵として外部に 'X' を入れておく。
+        vector<string> field(H + 1);
+        for (int h = 0; h < H; ++h) {
+            cin >> field[h];
+            field[h] += 'X';
+        }
+        field[H] = string(W + 1, 'X');
 
-		vector<vector<char> > field(H, vector<char>(W));
-		for (int h = 0; h < H; ++h) {
-			getline(cin, str);
-			for (int w = 0; w < W; ++w) {
-				field[h][w] = str[w];
-			}
-		}
-
-		// DP.
-		vector<vector<string> > work(H, vector<string>(W));
-
-		if (isdigit(field[H - 1][W - 1])) { 
-			work[H - 1][W - 1] = string(1, field[H - 1][W - 1]);
-		}
-
-		for (int w = W - 2; w >= 0; --w) {
-			if (isdigit(field[H - 1][w])) {
-				work[H - 1][w] = field[H - 1][w] + work[H - 1][w + 1];
-			}
-		}
-
-		for (int h = H - 2; h >= 0; --h) {
-			if (isdigit(field[h][W - 1])) {
-				work[h][W - 1] = field[h][W - 1] + work[h + 1][W - 1];
-			}
-		}
-
-		for (int h = H - 2; h >= 0; --h) {
-			for (int w = W - 2; w >= 0; --w) {
-				if (!isdigit(field[h][w])) { continue; }
-				if ((work[h][w + 1].size() > work[h + 1][w].size()) ||
-					((work[h][w + 1].size() == work[h + 1][w].size()) &&
-					 work[h][w + 1] > work[h + 1][w])) {
-					work[h][w] = field[h][w] + work[h][w + 1];
-				} else {
-					work[h][w] = field[h][w] + work[h + 1][w];
-				}
-			}
-		}
-#if 0
-		// test
-		for (int h = 0; h < H; ++h) {
-			for (int w = 0; w < W; ++w) {
-				cout << w << ", " << h << " : " << work[h][w] << endl;
-			}
-		}
-#endif
-		// cout << "hogehoge" << endl;
-
-		// shorten and get max
-		str = "";
-		for (int h = 0; h < H; ++h) {
-			for (int w = 0; w < W; ++w) {
-				int i = 0;
-				for (i = 0; i < work[h][w].size(); ++i) {
-					if (work[h][w][i] != '0') { break; }
-				}
-				work[h][w] = work[h][w].substr(i);
-				if (work[h][w].size() > str.size()) { str = work[h][w]; continue; }
-				if (work[h][w].size() == str.size()) {
-					if (work[h][w] > str) { str = work[h][w]; }
-				}
-			}
-		}
-
-		cout << str << endl;
-	}
+        cout << solve(field, H, W) << endl;
+    }
 }
+
